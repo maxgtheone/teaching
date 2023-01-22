68eb8adcbbd32fc3b54b6d9cd328d347936d9306
Max <maxgreenbergthompson@gmail.com>
2023-01-22 17:33:40 -0500

    list_comprehension

---
+++ review.py
@@ -0,0 +1,18 @@
+# List Comprehensions
+# Efficient way of looping, only use for simple loops,
+# returns lists and what it can return is up to you but usually an output of the thingy
+
+test_list_comprehension = [i for i in range(3)]
+
+# For loop equivalent
+return_list = []
+for i in range(3):
+    return_list.append(i)
+
+
+def add_one(i: int) -> int:
+    return i + 1
+
+#Create a list comprehension that utilizes the add_one function and returns a list [1,2,3]
+example_two_list_comprehension = []
+
