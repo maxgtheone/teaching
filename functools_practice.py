4102f8790f9235a28b7e1bb1f5b2bc8153993df0
Max <maxgreenbergthompson@gmail.com>
2023-01-29 16:53:30 -0500

    partial methods

--- functools_practice.py
+++ functools_practice.py
@@ -1,1 +1,39 @@
-#Give me an example of a higher order function
+# Give me an example of a higher order function
+
+from typing import Callable
+
+buy_strategy = Callable[[float], bool]
+
+
+def first_buy_strategy(price: float):
+    if price < 100:
+        should_buy = True
+    else:
+        should_buy = False
+    return should_buy
+
+
+def second_buy_strategy(price: float):
+    if price < 50:
+        should_buy = True
+    else:
+        should_buy = False
+    return should_buy
+
+
+def order_amount(should_buy: buy_strategy, price: float):
+    qty = 0
+    if should_buy(price):
+        qty = 10
+    return qty
+
+
+order_amount(first_buy_strategy, 75)
+
+#Partial functions from functools
+
+from functools import partial
+
+order_from_first_strategy = partial(order_amount, first_buy_strategy)
+
+order_from_first_strategy(75)
